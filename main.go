630b0d3497eaff4b31285112ecf732f8849c3865
eslerkang <eslerkang@gmail.com>
2022-01-26 06:09:29 +0000

    Created Account Struct & Funcs

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/eslerkang/learngo/accounts"
+)
+
 
 
 
 func main() {
-
+	account := accounts.NewAccount("kang")
+	account.Deposit(100)
+	fmt.Println(*account)
+	err:=account.WithDraw(110)
+	if err!=nil {
+		log.Fatal(err)
+	}
 }
